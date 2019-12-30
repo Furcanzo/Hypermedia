5ee03cdfdccaf36ee4deb880f7583cfc32e174f6
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-12-30 08:52:49 +0100

    Added signup route

--- api/api/controllers/user.js
+++ api/api/controllers/user.js
@@ -1,5 +1,6 @@
 const {Client} = require ('pg');
 const connect = require('../../server');
+const bcrypt = require('bcrypt');
 
 exports.userGetAll = (req, res, next)=>{
 	const client = new Client({
@@ -22,5 +23,39 @@ exports.userGetAll = (req, res, next)=>{
 			});
 			
 		});
+	});
+};
+
+exports.userPost = (req, res, next)=>{
+	const client = new Client({
+		connectionString: connect.connectString
+	});
+	client.connect((err, client, done)=>{
+		if (err){
+			const error = new Error('Cannot connect to DataBase');
+			error.status = 500;
+			return next(error);
+		}
+		bcrypt.hash(req.body.password, 10, (err, hash)=>{
+			if (err){
+				const error = new Error('Hashing error');
+				error.status = 500;
+				return next(error);
+			}else {
+				console.log(hash);
+				client.query('INSERT INTO users_web (email, password, admin)' +
+					'VALUES(\''+req.body.eMail+'\',\'' + hash +'\',\''+false+'\')', (err, result)=>{
+					if (err){
+						const error = new Error('Query error');
+						error.status = 500;
+						return next(error);
+					}
+					res.status(201).json({
+						message: 'user added'
+					});
+				});
+			}
+		});
+
 	});
 };
--- api/api/routes/user.js
+++ api/api/routes/user.js
@@ -5,4 +5,5 @@ const userController = require('../controllers/user');
 
 //list of routes for artist
 router.get('/', userController.userGetAll );
+router.post('/signup', userController.userPost )
 module.exports = router;
