7d03201d30baae2798c597f53c5be8e8556d19e7
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2020-01-04 11:09:05 +0100

    handling CORS error

--- api/app.js
+++ api/app.js
@@ -17,6 +17,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//prevent cors error
+app.use((req, res, next)=>{
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+	if (req.method === 'OPTIONS'){
+		res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE');
+		return res.status(200).json({});
+	}
+});
+
 //list of entities
 app.use('/artisticEvent', artisticEventRoutes);
 app.use('/performer', performerRoutes);
