c8164b18235fd970cd8fea6b41394940635fc8d4
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2020-01-05 15:28:36 +0100

    added basic_check-auth.js and added process variables

--- api/api/controllers/user.js
+++ api/api/controllers/user.js
@@ -82,33 +82,10 @@ exports.userLogin = (req, res, next)=>{
 					if (result && req.body.eMail === user.email) {
 						const token = jwt.sign({
 								email: user.email,
-								id: user.id
+								id: user.id,
+								admin: user.admin
 							},
-							"MIIEowIBAAKCAQEAts37yFhIbjDKFIqbjr9F+ZK3t8Lh7xJdelJOj6M6fM04ONLE" +
-							"Bl1F0ugfWvZ5D/RNtgrLW1/Xzq/oqmTr31/Gcojd56RtFehvojF5+gQ5sVJfX9dX" +
-							"J5Qqlq+e9RZT89z+/a7x+Y5pW87f9Z87PEzdFYWThDyc/TAelxXMiH/i2z5UbePw" +
-							"z6p7kjj66Rvq5zryRROpwhAbyVRzlmxcV+/OmgV9mywjDCp/Vb2jjS1KyMbHH4W3" +
-							"JwOthT7grrJzs0ogkyeqbMQ8ypFXaxS7StmOK86TKwIfA7we6zbTiApTJx7e2WdS" +
-							"qgeysjnEr5qyx9tl5e3qW+18GBQ4FsqxObx57wIDAQABAoIBAGEYw9XEXqR4sb17" +
-							"aejGW+qWLZMjplIP1Sr1sNjGgHhtoKE+AfTHciD2PkFYjTEWOHiq/xzHqZ8U/GYV" +
-							"ONvqyUNow0UYpwBt922VDg7oU6SF9ja38IkLQBvNqIbRl0H+bkpctBYzuG6VUYvr" +
-							"SdFbmkqmKpagg1pQRs9FOuB93QdKC9QUSOChlDjePtulkgeDOMbVg8RJfC3cen1S" +
-							"yxLWA5z9BOVAXDwdopbMufV/60krbL6h1dVXoZGjslFQqH6V+3HXtYJX5fUHJVLd" +
-							"zAdXLTHMpVysb95Y8dpJlgk8xKYrqACMOg91Basabn2apQ7sdjiv2g9iznqnWN64" +
-							"trMmKuECgYEA5omhd5HGzvLpY0YQmyB6POM4aJjNpjRmjmTW54gbNkEA7HMGLIHW" +
-							"T1vZ4HJxMgIUPFb+vqVcD+CBfljtNmT8wgdotOHz8gruiZRxBChzNPq6bE69Do89" +
-							"X4W+cDSRe60HK+0/PMlrho5oiAGoOpckIE+UKTQ5sXUbhoYNPi/3GckCgYEAyv64" +
-							"IkdjO/AWClG328NcUkqOYkk2N+3K7kjyMKBX+2qPcdr2yD/ccOVY2apdBAtDSc0V" +
-							"I4yhlCHts5s3aFknTcAKY4CB5NeDqeuexEpAMNqjdkp3exITOkawjptcYbqpCBFp" +
-							"TFDIfszgCWQWfEwP+Ek/oHqE32irJ4L9oF7aUfcCgYBJa7vI4Nx4PFSoEUBop0dG" +
-							"7PaiGUgljrq6ztLCmk5QUROCOn/U3OTsEUyqdsgmLRNUxEANZ4ySYeXb32Zs1GzL" +
-							"PqDSOsP+agzeNDBqKpfGX7PPmqQLZwMc0PN9uMA8qzaFfuGM8nkUOXaBGkYBovEW" +
-							"eUGufbuCENHbXgYJsCz6QQKBgQC+P8h3aAybKkKd3UTB1PD7tyCHATKtbFIazYcf" +
-							"B8gtvAtAnAVaXG/P50NEjSSsG5ou2uSK8syktr++6yQcyiHJ/sg9xTz4KvwdDtU4" +
-							"BpbuspsszFH6qFAY+BOVNAp7FaRCCX90pOmj3YPDiWiBihAVlSDq2KkbcU+nsl7e" +
-							"JMfctQKBgEPCN2oE8Ry1Md+vdMuFXG03plls8cd6UCwE5OszIgRH9T20+H9wIn8z" +
-							"AlUDPgSKGUjpj3DUIc+GgJbzU3rLPtOVRC3N/eis/CsO2/6jX9Vj2N2vRBU5bp50" +
-							"4AKyc/i21aYdY4cGcGbL6IXuPEObPg2doJumidv0UWdu24bw3uBN",
+							process.env.JWT_KEY,
 							{
 								expiresIn: '1h'
 							});
--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next)=>{
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', '*');
 	if (req.method === 'OPTIONS'){
-		res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE');
+		res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PATCH');
 		return res.status(200).json({});
 	}
 	next();
