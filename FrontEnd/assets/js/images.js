d73afac53aa85ca013f3fc1c58570a9a9385191f
MatteoBaragioli <53335403+MatteoBaragioli@users.noreply.github.com>
2020-02-01 11:15:56 +0100

    Update images.js

--- FrontEnd/assets/js/images.js
+++ FrontEnd/assets/js/images.js
@@ -11,13 +11,13 @@ xmlhttp.onreadystatechange = function() {
         	logo.setAttribute("src", myArr.assets[1].request.url);
         var favicon = document.getElementById("favicon");
         if(favicon!= null && favicon!=undefined)
-	        favicon.href = myArr.assets[3].request.url;
+	        favicon.href = myArr.assets[2].request.url;
 	    var event = document.getElementById("event_img");
         if(event!= null && event!=undefined)
-        	event.setAttribute("src", myArr.assets[4].request.url);
+        	event.setAttribute("src", myArr.assets[3].request.url);
         var performer = document.getElementById("performers_img");
         if(performer!= null && performer!=undefined)
-        	performer.setAttribute("src", myArr.assets[5].request.url);
+        	performer.setAttribute("src", myArr.assets[4].request.url);
     }
 };
 xmlhttp.open("GET", url, true);
