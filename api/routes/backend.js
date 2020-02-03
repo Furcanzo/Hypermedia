5dd4bda20e63d90dda3730dbd01531adfd51346a
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2020-02-03 19:37:35 +0100

    fix

--- api/routes/backend.js
+++ api/routes/backend.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+
+const inputfile = __dirname + '/../../documentation/spec.yaml',
+    outputfile = 'output.json',
+    yaml = require('js-yaml'),
+    fs = require('fs'),
+    obj = yaml.load(fs.readFileSync(inputfile, {encoding: 'utf-8'}));
 
 //list of routes for backend documentation
-router.get('swaggerui', (req, res, next)=>{
-    //todo
-});
+router.use('/swaggerui', swaggerUi.serve);
+router.get('/swaggerui', swaggerUi.setup(obj));
 
 router.get('/:file', (req, res, next)=>{
     res.sendFile(req.params.file, {root: __dirname + '/../../documentation'}, (err)=>{
